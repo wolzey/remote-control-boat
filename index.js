e7fa7a34b88d9bf59f8f66e67a9a1035d7ce3571
Ethan Wolz <ethan@creadiv.io>
2018-07-15 17:21:49 -0600

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+const express  = require('express')
+const socketio = require('socket.io')
+const http     = require('http')
+const SerialPort = require('serialport')
+
+const app = express()
+const server = http.Server(app)
+const port = process.env.PORT || 3001
+
+
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 }, (err) => {
+  if (err) {
+    console.log('Cannot connect')
+    return
+  }
+})
+
+const sendArduino = (data) => {
+  port.write(data)
+}
+
+server.listen(port, () => {
+  console.log(`Listening on ${port}`)
+  sendArduino('A')
+})
+
+io.on('connection', () => {
+  console.log('User connected')
+
+  socket.on('light control', (data) => {
+    sendArduino(data)
+  })
+})
